8189b2b91544f92f91a3553b8c515cc116163b59
Dheepak Krishnamurthy <me@kdheepak.com>
2023-07-22 02:58:04 -0400

    feat: Update docs ✨

--- docs/make.jl
+++ docs/make.jl
@@ -1,25 +1,20 @@
 using Crossterm
 using Documenter
 
-DocMeta.setdocmeta!(Crossterm, :DocTestSetup, :(using Crossterm); recursive=true)
+DocMeta.setdocmeta!(Crossterm, :DocTestSetup, :(using Crossterm); recursive = true)
 
 makedocs(;
-    modules=[Crossterm],
-    authors="Dheepak Krishnamurthy",
-    repo="https://github.com/kdheepak/Crossterm.jl/blob/{commit}{path}#{line}",
-    sitename="Crossterm.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://kdheepak.github.io/Crossterm.jl",
-        edit_link="main",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-    ],
+  modules = [Crossterm],
+  authors = "Dheepak Krishnamurthy",
+  repo = "https://github.com/kdheepak/Crossterm.jl/blob/{commit}{path}#{line}",
+  sitename = "Crossterm.jl",
+  format = Documenter.HTML(;
+    prettyurls = get(ENV, "CI", "false") == "true",
+    canonical = "https://kdheepak.com/Crossterm.jl",
+    edit_link = "main",
+    assets = String[],
+  ),
+  pages = ["Home" => "index.md"],
 )
 
-deploydocs(;
-    repo="github.com/kdheepak/Crossterm.jl",
-    devbranch="main",
-)
+deploydocs(; repo = "github.com/kdheepak/Crossterm.jl", devbranch = "main")
