3541167736235fc6491f6a5bba644fd58d3c8c2b
Documenter.jl <documenter@juliadocs.github.io>
2023-07-22 01:14:05 +0000

    build based on 18538f0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Crossterm","category":"page"},{"location":"#Crossterm","page":"Home","title":"Crossterm","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Crossterm.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Crossterm]","category":"page"}]
+}
